57573945b4d88e62cb14cf8300d62bf158e783aa
Juan Camilo Neira Campos <j.neira@uniandes.edu.co>
2023-11-25 18:26:48 -0500

    Update views.py

--- places/places/views.py
+++ places/places/views.py
@@ -18,4 +18,4 @@ def PlaceCreate(request):
         place = Place()
         place.name = data_json["name"]
         place.save()
-        return HttpResponse("successfully created variable")
+        return HttpResponse("successfully created place")
